3dfca0d7b630397db3656bc9b835cbe5a8e8db97
rawendra <rawendra@gmail.com>
2023-01-20 01:53:20 +0530

    template form setUpdate setPatch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('userForm') _userForm: NgForm;
+  answer: string = '';
+  genders = ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(userForm: NgForm) {
+    console.log(this._userForm);
+  }
+
+  autoFill() {
+    this._userForm.setValue({
+      userData: {
+        email: 'asd@asd.com',
+        username: 'rawendra',
+      },
+      questionAnswer: 'asdfg',
+      secret: 'pet',
+      gender: 'male',
+    });
+  }
+
+  updateName() {
+    this._userForm.form.patchValue({
+      userData: { username: 'erikcartman' },
+    });
   }
 }
