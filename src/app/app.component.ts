6a18458c5ba0a14a00bf29633e12feb19ba189bd
rawendra <rawendra@gmail.com>
2023-01-20 02:13:30 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('userForm') _userForm: NgForm;
-  answer: string = '';
   genders = ['male', 'female'];
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
-
-  onSubmit(userForm: NgForm) {
-    console.log(this._userForm);
-  }
-
-  autoFill() {
-    this._userForm.setValue({
-      userData: {
-        email: 'asd@asd.com',
-        username: 'rawendra',
-      },
-      questionAnswer: 'asdfg',
-      secret: 'pet',
-      gender: 'male',
-    });
-  }
-
-  updateName() {
-    this._userForm.form.patchValue({
-      userData: { username: 'erikcartman' },
-    });
-  }
 }
